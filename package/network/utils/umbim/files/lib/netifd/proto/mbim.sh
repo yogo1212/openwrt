4302694c1863f975f7563647b5f68dcb57fa22ba
Leon M. George <leon@georgemail.eu>
2022-10-11 17:18:58 +0200

    umbim: pass through ipXtable to child interfaces
    Analogous to e51aa699f7ca3ce83a0add622c0fd17d0caafc46.
    
    Signed-off-by: Leon M. Busch-George <leon@georgemail.eu>

--- package/network/utils/umbim/files/lib/netifd/proto/mbim.sh
+++ package/network/utils/umbim/files/lib/netifd/proto/mbim.sh
@@ -25,8 +25,8 @@ _proto_mbim_setup() {
 	local tid=2
 	local ret
 
-	local device apn pincode delay $PROTO_DEFAULT_OPTIONS
-	json_get_vars device apn pincode delay auth username password $PROTO_DEFAULT_OPTIONS
+	local device apn pincode delay ip4table ip6table $PROTO_DEFAULT_OPTIONS
+	json_get_vars device apn pincode delay auth username password ip4table ip6table $PROTO_DEFAULT_OPTIONS
 
 	[ -n "$ctl_device" ] && device=$ctl_device
 
@@ -144,6 +144,7 @@ _proto_mbim_setup() {
 	json_add_string name "${interface}_4"
 	json_add_string ifname "@$interface"
 	json_add_string proto "dhcp"
+	[ -n "$ip4table" ] && json_add_string ip4table "$ip4table"
 	proto_add_dynamic_defaults
 	json_close_object
 	ubus call network add_dynamic "$(json_dump)"
@@ -153,6 +154,7 @@ _proto_mbim_setup() {
 	json_add_string ifname "@$interface"
 	json_add_string proto "dhcpv6"
 	json_add_string extendprefix 1
+	[ -n "$ip6table" ] && json_add_string ip6table "$ip6table"
 	proto_add_dynamic_defaults
 	json_close_object
 	ubus call network add_dynamic "$(json_dump)"
